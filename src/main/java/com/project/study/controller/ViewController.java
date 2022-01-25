0da4b36e5eabf078904a5cc906a61ce98c55c4c2
최수연 <suyun329@naver.com>
2022-01-25 18:59:39 +0900

    first dashboard

---
+++ src/main/java/com/project/study/controller/ViewController.java
@@ -0,0 +1,24 @@
+package com.project.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("view/*")
+public class ViewController {
+
+	@RequestMapping("view/dashboard")
+	public ModelAndView dashboard() {
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+	
+	@RequestMapping("view/test")
+	public ModelAndView test() {
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>study 커뮤니티</title>
+</head>
+<body>
+<p>환영합니다 study 커뮤니티입니다.(index page)</p>
+<a href="/study/view/dashboard">대시보드 보러가기</a>
+<a href="/study/view/test">test page</a>
+</body>
+</html>
